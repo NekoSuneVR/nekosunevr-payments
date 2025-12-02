ed7c43beda693663de4c1dbb991e5f1ddfb90c2c
NekoSuneVR <chisdeal2014@hotmail.com>
2025-12-02 21:34:54 +0000

    Life savour

--- test/pol-getpayments.js
+++ test/pol-getpayments.js
@@ -1,11 +1,13 @@
-const {POLModule} = require("../lib/index.js")
+const { BNBModule } = require('../lib/index.js')
 
-async function test(account, amount, timestamp) {
-  const module = new POLModule({
-    tokenContract: '0x1379E8886A944d2D9d440b3d88DF536Aea08d9F3' // MYST
-  });
-  const transaction = await module.existsTransaction(account, amount, timestamp);
-  console.log(transaction);
+async function test (account, amount, timestamp) {
+  const module = new BNBModule({
+    tokenContract: '0x55d398326f99059fF775485246999027B3197955',
+    url: 'https://bsc1.trezor.io/api',
+    altExplorerUrls: ['https://bsc2.trezor.io/api']
+  })
+  const transaction = await module.existsTransaction(account, amount, timestamp)
+  console.log(transaction)
 }
 
-test('0x819321542ed9b59e99aac414abf1820854d725d7', '0.05', 1764707645);
+test('0x2CF64Ab6644EC9BA8A0289443e8e08bdDC77124D', '100.017396', 1764710562)
